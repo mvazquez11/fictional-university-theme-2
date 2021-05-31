926785bfc28a3b640748bde051e731c792325420
mvazquez11 <desarrollo@basico.fm>
2021-05-31 12:59:07 -0500

    Refactoring and gmaps integration added

--- functions.php
+++ functions.php
@@ -22,6 +22,40 @@ function university_files()
 
 add_action('wp_enqueue_scripts', 'university_files');
 
+
+//---pageBanner() - Function to dinamically process the content of the banner for each page and section
+function pageBanner($args = null)
+{
+  //---Php logic will live here
+  //---If there is no title defined it will take the default title value
+  if (!$args['title']) {
+    $args['title'] = get_the_title();
+  }
+
+  if (!$args['subtitle']) {
+    $args['subtitle'] = get_field('page_banner_subtitle');
+  }
+
+  if (!$args['photo']) {
+    if (get_field('page_banner_background_image') and !is_archive() and !is_home()) {
+      //---Pulls specific size of the image
+      $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+    } else {
+      $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+    }
+  }
+?>
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php echo $args['title']; ?></h1>
+      <div class="page-banner__intro">
+        <p><?php echo $args['subtitle']; ?></p>
+      </div>
+    </div>
+  </div>
+<?php }
+
 function university_features()
 {
   //---Add menu in navbar
@@ -32,6 +66,17 @@ function university_features()
   // register_nav_menu('footerLocationTwo', 'Footer Location Two');
   //---Add title to pages
   add_theme_support('title-tag');
+  //---Enable thumbnails for posts
+  add_theme_support('post-thumbnails');
+  //---Params
+  // 1 - reference name
+  // 2 - width
+  // 3- height
+  // 4 - crop image? (true/false)
+  add_image_size('professorLandscape', 400, 260, true);
+  add_image_size('professorPortrait', 480, 650, true);
+
+  add_image_size('pageBanner', 1500, 350, true);
 }
 
 add_action('after_setup_theme', 'university_features');
@@ -65,6 +110,16 @@ function university_adjust_queries($query)
 
 
 add_action('pre_get_posts', 'university_adjust_queries');
+
+//---Google Analytics
+//---This function adds the key configured in the Google API
+//---and then it is added as a field with value for ACF
+function universityMapKey($api)
+{
+  $api['key'] = 'AIzaSyBqnPePJ30hHNXnkCWPhwMW4L2sbhKDVo0';
+  return $api;
+}
+add_filter('acf/fields/google_map/api', 'universityMapKey');
 
 
 /* Shortcode test - Totally aside from the course */
