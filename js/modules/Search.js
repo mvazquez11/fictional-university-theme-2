8a93a27df785b94fcb7a51c5c43e304e7061079c
mvazquez11 <desarrollo@basico.fm>
2021-06-09 13:29:40 -0500

    WP API Funcionality to get content added

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -3,6 +3,8 @@ import $ from 'jquery';
 class Search {
   //---Describe our object
   constructor(){
+    //---Search layout loaded first
+    this.addSearchHTML();
     this.resultsDiv = $("#search-overlay__results");
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
@@ -43,7 +45,7 @@ class Search {
           this.resultsDiv.html('<div class="spinner-loader"></div>');
           this.isSpinnerVisible = true;
         }
-      this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+      this.typingTimer = setTimeout(this.getResults.bind(this), 750);
       } else {
         //---Prevents the spinner to be visible
         this.resultsDiv.html('');
@@ -55,19 +57,23 @@ class Search {
 
   //---Obtain search data
   getResults(){
-    $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val() , posts => {
-
-      this.resultsDiv.html(`
+    $.when(
+      $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val()),
+      $.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val())).then((posts, pages) => {
+      var combinedResults = posts[0].concat(pages[0]);
+        this.resultsDiv.html(`
         <h2 class="search-overlay__section-title">General Information</h2>
-        ${posts.length ? '<ul class="link-list min-list>' : '<p>No general information matches that search.</p>'  }
-          ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
-          ${posts.length ? '</ul>' : ''}
+        ${combinedResults.length ? '<ul class="link-list min-list>' : '<p>No general information matches that search.</p>'  }
+          ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+          ${combinedResults.length ? '</ul>' : ''}
         </ul>
       `);
       this.isSpinnerVisible = false;
+
+
+    }, () => {
+      this.resultsDiv.html('<p>Unexpected Error. Try again!</p>');
     });
-
-
     //this.resultsDiv.html("Imagine Real Search Results");
     //this.isSpinnerVisible = false;
   }
@@ -89,6 +95,8 @@ class Search {
     this.searchOverlay.addClass("search-overlay--active");
     //---Remove the ability to scroll
     $("body").addClass("body-no-scroll");
+    this.searchField.val('');
+    setTimeout(() => this.searchField.focus() ,301);
     console.log("Our open method just ran");
     this.isOverlayOpen = true;
   }
@@ -99,6 +107,24 @@ class Search {
     $("body").removeClass("body-no-scroll");
     console.log("Our close method just ran");
     this.isOverlayOpen = false;
+  }
+
+  addSearchHTML(){
+    $("body").append(`
+      <div class="search-overlay">
+        <div class="search-overlay__top">
+          <div class="container">
+            <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+            <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+            <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+          </div>
+        </div>
+        <div class="container">
+          <div id="search-overlay__results"></div>
+        </div>
+      </div>
+    `);
+
   }
 
 }
