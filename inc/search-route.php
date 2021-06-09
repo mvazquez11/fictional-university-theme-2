3908295be514f3a9b5f558ca21105e9756487dee
mvazquez11 <desarrollo@basico.fm>
2021-06-09 18:03:42 -0500

    Custom API for professors added

--- inc/search-route.php
+++ inc/search-route.php
@@ -2,6 +2,7 @@
 
 add_action('rest_api_init', 'universityRegisterSearch');
 
+//---Register new custom endpoint
 function universityRegisterSearch()
 {
   register_rest_route('university/v1', 'search', array(
@@ -10,7 +11,23 @@ function universityRegisterSearch()
   ));
 }
 
+//---Process the request
 function universitySearchResults()
 {
-  return "Congrats! Route created";
+  $professors = new WP_Query(array(
+    'post_type' => 'professor',
+  ));
+
+  $professorResults = array();
+
+  //---Find the professors and generate content that is taken from API
+  while ($professors->have_posts()) {
+    $professors->the_post();
+    array_push($professorResults, array(
+      'title' => get_the_title(),
+      'permalink' => get_the_permalink(),
+    ));
+  }
+
+  return $professorResults;
 }
