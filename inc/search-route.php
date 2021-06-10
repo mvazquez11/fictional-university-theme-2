50f487537def2a4ff2ed751777c2ab0e2a6ee9b5
mvazquez11 <desarrollo@basico.fm>
2021-06-10 15:37:48 -0500

    Routes for each post type content added

--- inc/search-route.php
+++ inc/search-route.php
@@ -12,22 +12,66 @@ function universityRegisterSearch()
 }
 
 //---Process the request
-function universitySearchResults()
+function universitySearchResults($data)
 {
-  $professors = new WP_Query(array(
-    'post_type' => 'professor',
+  $mainQuery = new WP_Query(array(
+    //---For single post
+    //'post_type' => 'professor',
+    //---For multiple posts
+    'post_type' => array('post', 'page', 'professor', 'program', 'campus', 'event'),
+    //---'s' as search
+    //---'term' is the param in the URL that stores the value to be searched
+    's' => sanitize_text_field($data['term'])
   ));
 
-  $professorResults = array();
+  $results = array(
+    'generalInfo' => array(),
+    'professors' => array(),
+    'programs' => array(),
+    'events' => array(),
+    'campuses' => array()
+  );
 
-  //---Find the professors and generate content that is taken from API
-  while ($professors->have_posts()) {
-    $professors->the_post();
-    array_push($professorResults, array(
-      'title' => get_the_title(),
-      'permalink' => get_the_permalink(),
-    ));
+  //---Find the post type and generate content that is taken from API
+  while ($mainQuery->have_posts()) {
+    $mainQuery->the_post();
+
+    //---Build the API Endpoint according to the content
+    if (get_post_type() == 'post' or get_post_type() == 'page') {
+      array_push($results['generalInfo'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+      ));
+    }
+
+    if (get_post_type() == 'professor') {
+      array_push($results['professors'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+      ));
+    }
+
+    if (get_post_type() == 'program') {
+      array_push($results['programs'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+      ));
+    }
+
+    if (get_post_type() == 'campus') {
+      array_push($results['campuses'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+      ));
+    }
+
+    if (get_post_type() == 'event') {
+      array_push($results['events'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+      ));
+    }
   }
 
-  return $professorResults;
+  return $results;
 }
