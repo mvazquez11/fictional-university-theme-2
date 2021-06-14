1e6b7abef52e8241f37e4d39cbc6d05c568bcba1
mvazquez11 <desarrollo@basico.fm>
2021-06-14 13:26:46 -0500

    Search funcionality enhanced

--- inc/search-route.php
+++ inc/search-route.php
@@ -56,6 +56,16 @@ function universitySearchResults($data)
     }
 
     if (get_post_type() == 'program') {
+      $relatedCampuses = get_field('related_campus');
+      if ($relatedCampuses) {
+        foreach ($relatedCampuses as $campus) {
+          array_push($results['campuses'], array(
+            'title' => get_the_title($campus),
+            'permalink' => get_the_permalink($campus)
+          ));
+        }
+      }
+
       array_push($results['programs'], array(
         'title' => get_the_title(),
         'permalink' => get_the_permalink(),
@@ -107,12 +117,31 @@ function universitySearchResults($data)
 
     //---Set relation professor-program to display it in search
     $programRelationshipQuery = new WP_Query(array(
-      'post_query' => 'professor',
+      'post_query' => array('professor', 'event'),
       'meta_query' => $programsMetaQuery
     ));
 
     while ($programRelationshipQuery->have_posts()) {
       $programRelationshipQuery->the_post();
+
+      if (get_post_type() == 'event') {
+        $eventDate = new DateTime(get_field('event_date'));
+        $description = null;
+        if (has_excerpt()) {
+          $description = get_the_excerpt();
+        } else {
+          $description = wp_trim_words(get_the_content(), 18);
+        };
+
+        array_push($results['events'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink(),
+          'month' => $eventDate->format('M'),
+          'day' => $eventDate->format('d'),
+          'description' => $description
+
+        ));
+      }
 
       if (get_post_type() == 'professor') {
         //---0 - Default, size:
@@ -127,6 +156,8 @@ function universitySearchResults($data)
 
     //---Remove duplicates with array_unique
     $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+    $results['events'] = array_values(array_unique($results['events'], SORT_REGULAR));
+
 
     return $results;
   }
